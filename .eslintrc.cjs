235a3f57cf184fc06d56c2934dd2461597c67bb3
Joe Bailey <bailejl@yahoo.com>
2022-11-03 18:51:27 -0700

    Moved to Svelte Kit.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,50 +1,20 @@
 module.exports = {
-  extends: ["eslint:recommended", "prettier"],
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    // project: ["./vite.config.ts"],
-    ecmaVersion: 2019,
-    sourceType: "module",
-    impliedStrict: true,
-    ecmaFeatures: {
-      jsx: false,
-    },
-  },
-  plugins: ["@typescript-eslint", "svelte3"],
-  root: true,
-  env: {
-    es6: true,
-    browser: true,
-  },
-  overrides: [
-    {
-      files: ["*.svelte"],
-      processor: "svelte3/svelte3",
-    },
-    {
-      files: ["**/*.ts"],
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: ["./tsconfig.json"],
-      },
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:@typescript-eslint/strict",
-        "prettier",
-      ],
-      rules: {
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-namespace": "off",
-        "@typescript-eslint/no-unused-vars": [
-          "error",
-          { argsIgnorePattern: "^_" },
-        ],
-      },
-    },
-  ],
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	ignorePatterns: ['*.cjs'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript')
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	}
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import eslint from "vite-plugin-eslint";
+import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  root: "web",
-  build: {
-    outDir: "../dist/web",
-    emptyOutDir: true,
-    assetsDir: "",
-    // sourcemap: 'inline', // enable for debugging
-  },
-  server: {
-    port: 4200,
-  },
-  plugins: [svelte(), eslint()],
-});
+const config: UserConfig = {
+	plugins: [sveltekit()]
+};
+
+export default config;
