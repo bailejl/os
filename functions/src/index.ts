b5d6887351dfcce4aeeacaa9d34af4d47a4a05e9
Joe Bailey <bailejl@yahoo.com>
2023-01-02 09:56:19 -0800

    Set up firebase correctly in the repo along with emulators.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions';
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export function analysisFunction(request: functions.Request, response: functions.Response): void {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
+}
